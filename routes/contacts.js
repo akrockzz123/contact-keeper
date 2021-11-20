27a4612f1e040f4fc04f706e5f162ac6f44bd44f
akrockzz123 <aniketkumar1601@gmail.com>
2021-11-20 20:05:51 +0530

    added various routes of contact

--- routes/contacts.js
+++ routes/contacts.js
@@ -6,6 +6,7 @@ const auth = require("../middleware/auth")
 
 const Contact = require("../models/contact")
 
+const { validationResult,check } = require('express-validator');
 // @route GET api/contacts
 //@desc get all contacts of logged in user
 //@access Private
@@ -29,15 +30,97 @@ router.get('/',auth,async (req,res) => {
 // @route POST api/contacts
 //@desc Add new contact
 //@access Private
-router.post('/',auth,(req,res) => {
-    res.send('Add contact of user');
+router.post('/',auth,[
+    check('name','please enter name'),
+    check('email', 'please include a valid email').isEmail(),
+    check('phone','Please enter valid contact number').isLength({ min: 6 }),
+check('type',"please enter type of contact")],async (req,res) => {
+
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { name,email,phone,type,date } = req.body
+
+    try {
+
+        let users = await Contact.findOne({ name })
+
+        if(users) {
+
+            res.status(400).json({ msg: "Contact already exist "})
+        }
+
+        const contact = new Contact({
+            name,
+            email,
+            phone,
+            type,
+            date,
+            user: req.user.id
+        });
+
+      const con =  await contact.save()
+
+       
+        res.json(con)
+
+    } catch (err) {
+        
+        console.error(err.message)
+        
+        res.status(500).send('Server error')
+    }
 });
 
 // @route PUT api/contacts/:id
 //@desc update contact of user
 //@access Private
-router.put('/:id',(req,res) => {
-    res.send('Update contact of user');
+router.put('/:id',auth,async (req,res) => {
+    const { name,email,phone,type } = req.body
+
+    // build a contact object
+
+    const contactFields = {}
+
+    if(name) contactFields.name = name
+
+    if(email) contactFields.email = email
+    if(phone) contactFields.phone = phone
+    if(type) contactFields.type = type;
+
+    try {
+        
+        const contact = await Contact.findById(req.params.id)
+
+        if(!contact)
+        {
+            res.status(404).json({ msg: "Contact doesnot exist"})
+        }
+
+        if(contact.user.toString() !== req.user.id)
+        {
+            res.status(401).json({ msg: "Not authorized to change this contact"})
+        }
+        else{
+            const contac = await Contact.findByIdAndUpdate(req.params.id,
+                {
+                    $set: contactFields
+                },{
+                    new: true
+                });
+
+                res.json(contac)
+        }
+
+    } catch (err) {
+        console.error(err.message)
+
+        res.status(500).send("Server Error")
+    }
 });
 
 // @route DELETE api/contacts/:id
